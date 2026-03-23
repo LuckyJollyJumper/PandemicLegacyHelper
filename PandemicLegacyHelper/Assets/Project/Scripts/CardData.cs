a32478c6089f34cf5eaa3ed7ca9b10463e6a0e5a
Luc <91373173+LuckyJollyJumper@users.noreply.github.com>
2026-03-23 21:43:19 +0100

    Small update in UI

--- PandemicLegacyHelper/Assets/Project/Scripts/CardData.cs
+++ PandemicLegacyHelper/Assets/Project/Scripts/CardData.cs
@@ -32,10 +32,10 @@ public class CardData
     /// <returns></returns>
     public Color GetCardColour(){
         return this.Colour switch{
-            CardData.CardColour.Blue => new Color(0.284f, 0.742f, 0.893f),
-            CardData.CardColour.Black => new Color(0.284f, 0.284f, 0.284f),
-            CardData.CardColour.Yellow => new Color(0.953f, 0.973f, 0.333f),
-            CardData.CardColour.Green => new Color(0.24f, 0.56f, 0.302f),
+            CardData.CardColour.Blue => new Color(0.284f, 0.742f, 0.893f), //#48BDE4
+            CardData.CardColour.Black => new Color(0.284f, 0.284f, 0.284f), //#484848
+            CardData.CardColour.Yellow => new Color(0.953f, 0.973f, 0.333f), //#F3F855
+            CardData.CardColour.Green => new Color(0.24f, 0.56f, 0.302f), //#3D8F4D
             _ => new Color(64f,64f,64f)
         };
     }
